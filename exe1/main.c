4bea9395430c46c586483670e8ea83afd95b89f4
felipebns <felipebns@al.insper.edu.br>
2025-04-14 09:35:21 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -30,6 +30,10 @@ void i2c_task(void *p) {
 
     // TODO
     // Configure o acc para operar em 4G
+    uint8_t buf_write_accel[2];
+    buf_write_accel[0] = MPUREG_ACCEL_CONFIG; // registrador
+    buf_write_accel[1] = 1 << 3;            // valor
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_write_accel, 2, false);
 
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
