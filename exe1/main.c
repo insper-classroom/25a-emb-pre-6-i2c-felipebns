a2a48bf24920bd460fe3cf46b2e63e5a6f5567a1
felipebns <felipebns@al.insper.edu.br>
2025-04-14 09:50:04 -0300

    ex1

--- exe1/main.c
+++ exe1/main.c
@@ -30,9 +30,10 @@ void i2c_task(void *p) {
 
     // TODO
     // Configure o acc para operar em 4G
-    uint8_t buf_write_accel[2];
-    buf_write_accel[0] = MPUREG_ACCEL_CONFIG; // registrador
-    buf_write_accel[5] = 1 << 7;            // valor
+    // Configure o ACCEL_CONFIG para operar em ±4g (bits [4:3] = 01)
+    uint8_t buf_write_accel[2]; //é um vetor de ints, deve-se lembrar disso para configurar corretamente
+    buf_write_accel[0] = MPUREG_ACCEL_CONFIG;
+    buf_write_accel[1] = 0x08;  // 0000 1000 -> bit 3 = 1, 1 elemento tem 8 bits 
     i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, buf_write_accel, 2, false);
 
     while (1) {
