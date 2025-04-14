5b5f43b679b31f3012ddbdb93f81e9f75e75f128
felipebns <felipebns@al.insper.edu.br>
2025-04-14 10:04:04 -0300

    ex3

--- exe3/main.c
+++ exe3/main.c
@@ -23,8 +23,12 @@ void i2c_task(void *p) {
 
     // TODO
     // read id chip BMP280
+    uint8_t buffer[1];
+    uint8_t bmp280_id_reg = 0xD0;  // endereço do registrador bmp280 ID
+    i2c_write_blocking(i2c_default, I2C_CHIP_ADDRESS, &bmp280_id_reg, 1, true);
+    i2c_read_blocking(i2c_default, I2C_CHIP_ADDRESS, buffer, 1, false);
     printf("BMP280 ID: 0x%X \n", buffer[0]);
-
+    
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(200));
     }
